d89a6daf015553c35820015d42e5bed39f314ad0
unknown <Zilikaks>
2019-12-13 08:10:57 +0300

    Add spawn and fix animation

---
+++ DuckHunt/Assets/Scripts/Bullets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    public Sprite[] SpriteList;
+    private Sprite bullets;
+    void Start()
+    {
+        bullets = this.GetComponent<SpriteRenderer>().sprite;
+    }
+    
+    void Update()
+    {
+        if (GetComponent<MoveDuck>().BulletCount == 3)
+        {
+            bullets = SpriteList[0];
+        }
+        if (GetComponent<MoveDuck>().BulletCount == 2)
+        {
+            bullets = SpriteList[1];
+        }
+        if (GetComponent<MoveDuck>().BulletCount == 1)
+        {
+            bullets = SpriteList[2];
+        }
+        if (GetComponent<MoveDuck>().BulletCount == 0)
+        {
+            bullets = SpriteList[3];
+        }
+    }
+}
--- DuckHunt/Assets/Scripts/MoveDuck.cs
+++ DuckHunt/Assets/Scripts/MoveDuck.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class MoveDuck : MonoBehaviour
 {
+    public int BulletCount;
+    public GameObject[] SpawnList;
     Vector3 direction;
     Rigidbody2D rb;
     private Animator animator;
     public float speed = 1;
+    public GameObject currentSpawn;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        int rand = UnityEngine.Random.Range(0,4);
         direction = Vector3.zero;
-        switch (rand)
+        switch (currentSpawn.transform.position.x)
         {
             case 0:
                 {
@@ -53,6 +55,7 @@ public class MoveDuck : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && collision.gameObject.CompareTag("Scope"))
         {
             direction = new Vector3(0f, -1f, 0f);
+            BulletCount--;
         }
     }
 
@@ -60,7 +63,32 @@ public class MoveDuck : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Background"))
         {
+            int rand = UnityEngine.Random.Range(0, 4);
+            switch (rand)
+            {
+                case 0:
+                    {
+                        GetComponent<SpawnDuck>().Spawn(SpawnList[UnityEngine.Random.Range(0, 2)]);
+                        break;
+                    }
+                case 1:
+                    {
+                        GetComponent<SpawnDuck>().Spawn(SpawnList[UnityEngine.Random.Range(2, 4)]);
+                        break;
+                    }
+                case 2:
+                    {
+                        GetComponent<SpawnDuck>().Spawn(SpawnList[4]);
+                        break;
+                    }
+                case 3:
+                    {
+                        GetComponent<SpawnDuck>().Spawn(SpawnList[5]);
+                        break;
+                    }
+            }
             Destroy(this.gameObject);
+            Score.CurrentScore++;
         }
     }
 }
