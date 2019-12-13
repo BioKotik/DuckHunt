e15c2dd517bd77ee7fba35d6f781cb41771b4054
unknown <Zilikaks>
2019-12-13 09:07:00 +0300

    Add spawn and fix animations

---
+++ DuckHunt/Assets/Scripts/Bullets.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    public int bulletCount;
+    public Sprite[] SpriteList;
+    void Start()
+    {
+        bulletCount = 3;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && bulletCount > 0)
+        {
+            bulletCount--;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            bulletCount = 3;
+        }
+
+        if (bulletCount == 3)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = SpriteList[0];
+        }
+        if (bulletCount == 2)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = SpriteList[1];
+        }
+        if (bulletCount == 1)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = SpriteList[2];
+        }
+        if (bulletCount == 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = SpriteList[3];
+        }
+    }
+}
